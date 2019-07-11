4c29bece2caf8ec21d45a5fda0129ff99c0e9fad
s1260012 <s1260012@std6dc4.u-aizu.ac.jp>
2019-07-11 19:50:44 +0900

    add DiceRoll

---
+++ DiceRoll.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int roll(){
+  return  rand()%6 + 1;
+}
+
+int main(){
+  int die1,die2;
+  char name[10];
+ srand(time(NULL));
+ 
+  printf("Rolling the dice...\n");
+  die1 = roll();
+  die2 = roll();
+  printf("Die 1: %d\n",die1);
+  printf("Die 2: %d\n",die2);
+  printf("Total value: %d\n",die1 + die2);
+
+
+  
+  return 0;
+}
