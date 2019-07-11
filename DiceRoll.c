8989ee27c899b89033a6346fa4d7488ac57ddcb1
s1260012 <s1260012@std6dc4.u-aizu.ac.jp>
2019-07-11 20:05:29 +0900

    add dice

--- DiceRoll.c
+++ DiceRoll.c
@@ -7,7 +7,7 @@ int roll(){
 }
 
 int main(){
-  int die1,die2;
+  int die1,die2,die3;
   char name[10];
  srand(time(NULL));
  
@@ -19,15 +19,17 @@ printf("What is your name?\n");
   printf("Rolling the dice...\n");
   die1 = roll();
   die2 = roll();
+  die3 = roll();
   printf("Die 1: %d\n",die1);
   printf("Die 2: %d\n",die2);
-  printf("Total value: %d\n",die1 + die2);
+  printf("Die 3: %d\n",die3);
+  printf("Total value: %d\n",die1 + die2 + die3);
 
  
-  if(sie1+die2>7)
-    printf("You won\n");
+  if(die1+die2+die3>10)
+    printf("%s won\n",name);
   else
-    printf("You lost\n");
+    printf("%s lost\n",name);
   
   return 0;
 }
